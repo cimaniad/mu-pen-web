fa0324654d187790a058864851116ffdb0e6c040
Tiago <Tiago@Tiago>
2015-04-24 20:22:16 +0100

    commit inicial do projeto

--- api/objects/Patient.php
+++ api/objects/Patient.php
@@ -91,7 +91,7 @@ function saveEditPatient($params){
         $response['error'] = FALSE;
         $response['msg'] = mysql_error($connection);
     } else {
-        $response['cod'] = 300;
+        $response['cod'] = 500;
         $response['error'] = TRUE;
         $response['msg'] = mysql_error($connection);
     }
